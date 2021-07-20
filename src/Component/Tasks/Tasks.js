3acf9be59a9c86a179f8cf1068247ef51baf5500
Vishnu Paul <paulvishnu567@gmail.com>
2021-07-20 17:34:52 +0530

    inital commit

---
+++ src/Component/Tasks/Tasks.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AiFillDelete } from 'react-icons/ai';
+
+const Tasks = (submitTask) => {
+  const [tasks, setTasks] = useState([]);
+
+  const deleteTask = async (_id) => {
+    console.log(_id);
+    const res = await axios.delete('/tasks/' + _id);
+    fatchTask();
+  };
+  const fatchTask = async () => {
+    const responce = await axios.get('/tasks');
+    setTasks(responce.data);
+  };
+  useEffect(() => {
+    fatchTask();
+  }, [submitTask]);
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => {
+          const { _id, desc, date, category } = task;
+          return (
+            <div>
+              <li className='tasks'>
+                <div className='task-left-side'>
+                  <span className='delete '>
+                    <AiFillDelete onClick={() => deleteTask(_id)} />
+                  </span>
+                  <div className='details'>
+                    <p className='task'>{desc}</p>
+                    <p className='date'>{date}</p>
+                  </div>
+                </div>
+                <button className=' category-btn'>{category}</button>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Tasks;
---
+++ src/Component/inputs/Inputs.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+import axios from 'axios';
+import Tasks from '../Tasks/Tasks';
+
+const Inputs = () => {
+  const [desc, setDesc] = useState('');
+  const [date, setDate] = useState('');
+  const [category, setCategory] = useState('');
+
+  const submitTask = async (e) => {
+    e.preventDefault();
+    try {
+      const newTask = {
+        desc,
+        date,
+        category,
+      };
+      const res = await axios.post('/tasks', newTask);
+      setDesc('');
+
+      setDate('');
+      window.alert('post');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div className='inputs'>
+        <form method='POST' className='contact-form'>
+          <div className='section-title contact-head'>
+            <h2>
+              Todo<span>App</span>
+            </h2>
+          </div>
+          {/* form control */}
+          <div className='form-control'>
+            <label htmlFor='description'>Description</label>
+            <input
+              type='text'
+              placeholder='What do you need to do ?'
+              name='desc'
+              className='form-input'
+              id='description'
+              required
+              value={desc}
+              onChange={(e) => {
+                setDesc(e.target.value);
+              }}
+            />
+          </div>
+          <div className='sub-inputs'>
+            <div className='form-control mid'>
+              <label htmlFor='category'>Category</label>
+              <select
+                id='category'
+                name='category'
+                className='form-input category'
+                required
+                onChange={(e) => {
+                  const selectCategory = e.target.value;
+                  setCategory(selectCategory);
+                }}
+              >
+                <option value=''>Choose a category</option>
+                <option value='Personal'>Personal</option>
+                <option value='Work'>Work</option>
+                <option value='School'>School</option>
+                <option value='Office'>Office</option>
+                <option value='Other'>Other</option>
+              </select>
+            </div>
+
+            <div className='form-control mid'>
+              <label htmlFor='due-date'>Due date</label>
+              <input
+                type='date'
+                name='date'
+                className='form-input '
+                id='due-date'
+                required
+                value={date}
+                onChange={(e) => {
+                  setDate(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className='buttons'>
+            <button type='submit' className='btn btn-add' onClick={submitTask}>
+              Add task
+            </button>
+          </div>
+        </form>
+      </div>
+      <Tasks submitTask={submitTask} />
+    </div>
+  );
+};
+
+export default Inputs;
